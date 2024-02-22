564d04b97953415979d6fda30a3e78c90d3ebc1b
Kush Dalal <kushdalal007@gmail.com>
2024-02-22 14:41:55 -0500

    db changes

--- TMDB_Comments/DatabaseBuilder.py
+++ TMDB_Comments/DatabaseBuilder.py
@@ -6,6 +6,12 @@ def get_date():
     today = date.today()
     return today.strftime("%m_%d_%Y")
 
+def write_to_database(comment, movie_id):
+    # write the comment andm ovie id to the database in json format
+    with open('TMDB_Comments/Database.json', 'a') as f:
+        f.write(json.dumps({"movie_id": movie_id, "comment": comment}) + '\n')
+        f.close()
+
 
 headers = {
     "accept": "application/json",
@@ -20,7 +26,7 @@ with open('TMDB_Comments/movie_ids_' + get_date() + '.json', 'r') as f:
         data = json.loads(line)
         movie_ids.append(data['id'])
 
-id = 11
+id = 331
 
 url = "https://api.themoviedb.org/3/movie/" + str(id) + "/reviews?language=en-US&page=1"
 response = requests.get(url, headers=headers)
@@ -29,5 +35,9 @@ if response.json()['total_results'] < 0:
     print("No reviews for movie with id " + str(id))
 else:
     print(response.json())
-    with open('TMDB_Comments/ReviewDatabase.json', 'w') as f:
-        json.dump(response.json(), f)
+    # add just the comment content and movie id to the database
+    for review in response.json()['results']:
+        write_to_database(review['content'], id)
+
+
+

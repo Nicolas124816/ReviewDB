08c1e48dc695ac9468cfca6d51c8e1319450631c
Lang Min <min.276@osu.edu>
2024-03-28 10:56:35 -0400

    project links updated, images not showing

---
+++ ReviewDB/src/app/component/field-components/icons/case-icon/case-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-icon',
+  templateUrl: './case-icon.component.html',
+  styleUrl: './case-icon.component.css'
+})
+export class CaseIconComponent {
+
+}
---
+++ ReviewDB/src/app/component/field-components/icons/case-icon/case-icon.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CaseIconComponent } from './case-icon.component';
+
+@NgModule({
+  declarations: [
+    CaseIconComponent,
+  ],
+  exports: [
+    CaseIconComponent,
+  ],
+})
+export class CaseIconModule { }
